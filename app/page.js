8c23e69c86428919fc7ebb2df2b66ff52844b5b0
adhhamdev <msmadhhamg@gmail.com>
2024-03-20 21:33:19 +0530

    Add links to card components

--- app/page.js
+++ app/page.js
@@ -6,53 +6,63 @@ export default function Home() {
   const cards = [
     {
       image: "/convert-file.png",
+      href: 'convert-file',
       title: "Convert File",
       description: "Convert files from one format to another.",
     },
     {
       image: "/capture-website.png",
+      href: 'capture-website',
       title: "Capture Website",
       description: "Capture websites and save them as images or PDFs.",
     },
     {
       image: "/optimize-file.png",
+      href: 'optimize-file',
       title: "Optimize File",
       description:
         "Optimize files to reduce their size without losing quality.",
     },
     {
       image: "/create-thumbnail.png",
+      href: 'create-thumbnail',
       title: "Create Thumbnail",
       description: "Generate thumbnail images from larger images.",
     },
     {
       image: "/add-watermark.png",
+      href: 'add-watermark',
       title: "Add Watermark",
       description:
         "Add watermarks to protect your files from unauthorized use.",
     },
     {
       image: "/get-metadata.png",
+      href: 'get-metadata',
       title: "Get Metadata",
       description: "Retrieve metadata information from files.",
     },
     {
       image: "/write-metadata.png",
+      href: 'write-metadata',
       title: "Write Metadata",
       description: "Write or modify metadata information in files.",
     },
     {
       image: "/merge-files.png",
+      href: 'merge-files',
       title: "Merge Files",
       description: "Merge multiple files into a single file.",
     },
     {
       image: "/create-archive.png",
+      href: 'create-archive',
       title: "Create Archive",
       description: "Create compressed archives of files and folders.",
     },
     {
       image: "/execute-command.png",
+      href: 'execute-command',
       title: "Execute Command",
       description: "Execute custom commands or scripts.",
     },
@@ -61,13 +71,13 @@ export default function Home() {
   return (
     <>
       <header
-        className="bg-fixed py-4 bg-background-image bg-cover bg-no-repeat flex items-center rounded-b-xl h-96 md:h-96"
+        className="bg-fixed py-4 bg-background-image bg-contain bg-no-repeat md:bg-cover flex items-center rounded-b-xl h-64 md:h-96"
         style={{ boxShadow: "inset 0 -2px 10px 2px rgba(0, 0, 0, .2)" }}
       >
-        <div className="container md:ml-32">
-          <h1 className="flex items-center text-2xl text-center md:text-5xl md:text-left font-bold text-white">
+        <div className="container md:ml-32 flex flex-col justify-center md:block">
+          <h1 className="flex items-center text-2xl text-center md:text-5xl md:text-left font-bold text-white mx-auto md:mx-0 mb-5">
             <Image src={icon} width={50} alt="Logo" />
-            <span className="ml-4">FileDex.</span>
+            <span className="ml-0 md:ml-4">FileDex.</span>
           </h1>
           <p className="mx-auto text-center text-wrap w-2/3 md:text-left md:mx-0 md:text-3xl text-gray-200 ">
             Find the right tool for your file management needs.
@@ -85,27 +95,25 @@ export default function Home() {
             alt=""
             width={512}
             height={512}
-            className="absolute right-20 bottom-1/2 w-64 md:right-40 md:bottom-80 md:w-96"
+            className="absolute right-10 bottom-2/3 w-32 md:right-40 md:bottom-80 md:w-96"
           />
         </div>
       </header>
       <main>
-        <section id="tools" className="mt-60 md:mt-32">
-          <h1 className="ml-12 text-2xl">File Management Tools</h1>
+        <section id="tools" className="mt-32 md:mt-32">
+          <h1 className="text-slate-700 ml-12 text-2xl font-bold">File Management Tools</h1>
           <hr className="rounded-md h-2 bg-slate-300 w-32 ml-12 mt-2" />
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10 my-8">
             {cards.map((card, index) => (
               <Card
                 key={index}
-                image={card.image}
-                title={card.title}
-                description={card.description}
+                card={card}
               />
             ))}
           </div>
         </section>
       </main>
-      <footer className="bg-slate-900 py-4 bg-background-image bg-cover bg-fixed">
+      <footer className="bg-gray-700 py-4">
         <section className="py-8">
           <div className="container">
             <h2 className="text-2xl text-center text-white font-bold mb-4">Connect with me</h2>
--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,22 @@
 import Image from "next/image";
+import Link from 'next/link';
 
-const Card = ({ image, title, description }) => {
+const Card = ({ card }) => {
   return (
     <div className="bg-white shadow-lg rounded-xl p-4 bg-opacity-20 filter bg-gradient-to-b from-white to-slate-200 border border-gray-300 pt-10 hover:-translate-y-2 hover:shadow-xl transition duration-300">
       <Image
-        src={image}
-        alt={description}
+        src={card.image}
+        alt={card.description}
         className="w-32 object-contain rounded-lg mx-auto"
         width={512}
         height={512}
       />
-      <h2 className="text-xl mt-8">{title}</h2>
-      <p className="overflow-hidden overflow-ellipsis h-12">{description}</p>
+      <h2 className="text-xl mt-8 font-bold">{card.title}</h2>
+      <p title={card.description} className="overflow-hidden overflow-ellipsis h-12">{card.description}</p>
       <div className="flex justify-between mt-4">
-        <button className="hover:bg-slate-900 hover:text-white font-bold py-2 px-4 rounded-full border-2 border-slate-900 w-full">
-          {title.split(" ")[0]}
-        </button>
+        <Link href={card.href} className="text-center hover:bg-slate-900 hover:text-white font-bold py-2 px-4 rounded-full border-2 border-slate-900 w-full">
+          {card.title.split(" ")[0]}
+        </Link>
       </div>
     </div>
   );
